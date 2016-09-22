4620aae5687162cb935fbf7faa9382fe3a1c308c
ouxiaolong <ouxiaolong@cmcm.com>
2016-09-22 13:49:54 +0800

    add push script

--- public/push.sh
+++ public/push.sh
@@ -4,4 +4,5 @@
 grep -E --color=no -o "https:.*?</loc>" baidusitemap.xml | sed "s/<\/loc>//" > urls.txt
 
 #submit urls.txt file to baiduspider
-curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://blog.nofile.cc&token=YIxiIeMgg0wocEEP"
+curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://blog.nofile.cc&token=YIxiIeMgg0wocEEP" >> push.log
+echo `date` >> push.log
