dfd73c75bb0c736bfcc3840d15911d4c4aa31b56
ouxiaolong <ouxiaolong@cmcm.com>
2016-09-22 13:44:49 +0800

    submit urls.txt for baidu engine

--- public/push.sh
+++ public/push.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
+
+# generate newest urls.txt
+grep -E --color=no -o "https:.*?</loc>" baidusitemap.xml | sed "s/<\/loc>//" > urls.txt
+
+#submit urls.txt file to baiduspider
 curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://blog.nofile.cc&token=YIxiIeMgg0wocEEP"
