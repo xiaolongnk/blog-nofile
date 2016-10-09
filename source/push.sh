33e524df191810cb6ec88f8b725d78e8bbdbae5e
ouxiaolong <ouxiaolong@cmcm.com>
2016-10-09 19:33:52 +0800

    add method for gen url

--- source/push.sh
+++ source/push.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+grep -E --color=no -o "https:.*?</loc>" baidusitemap.xml | sed "s/<\/loc>//" > urls.txt
 curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://blog.nofile.cc&token=YIxiIeMgg0wocEEP"
